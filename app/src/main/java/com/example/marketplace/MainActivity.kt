e4eafbdcfb7f3d8b48fe414dd4f2e0b932fe96ec
Nodir <mahkamov.nodirbek.95@gmail.com>
2022-04-26 19:13:32 +0500

    Before moving into TabLayout

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,33 +21,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottom_navigation.itemIconTintList = null
+        setSupportActionBar(toolbar)
+
         supportFragmentManager.beginTransaction().replace(R.id.nav_container, FoodMenuFragment())
             .commit()
         val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNav.setOnItemSelectedListener (navListener)
+        bottomNav.setOnItemSelectedListener(navListener)
 
         val spOrderPlaces = findViewById<Spinner>(R.id.orderPlace)
-        spOrderPlaces.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val namesOfOrderPlaces =
+            listOf<String>("1Pat Tat St, San Po Kong","Maskva kalxoz", "Navoiy kalxoz", "Saraylik", "Xayraobod", "Olmazor")
+        val adapter = ArrayAdapter(this, R.layout.spinner_color, namesOfOrderPlaces)
+        adapter.setDropDownViewResource(R.layout.dropdown_spinner_color)
+        spOrderPlaces.adapter = adapter
+
+        spOrderPlaces.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
             }
+
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
-
     }
+
     private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
-        when(it.itemId){
+        when (it.itemId) {
             R.id.miLocation -> {
                 currentFragment = LocationFragment()
             }
-            R.id.miFoodMenu -> {
+            R.id.miOrderMenu -> {
                 currentFragment = FoodMenuFragment()
             }
-            R.id.miAccount  -> {
+            R.id.miAccount -> {
                 currentFragment = AccountFragment()
             }
         }
-        supportFragmentManager.beginTransaction().replace(R.id.nav_container, currentFragment).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.nav_container, currentFragment)
+            .commit()
         true
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miQRScanner -> Toast.makeText(this, "QR scanner is clicked", Toast.LENGTH_SHORT).show()
+            R.id.miSearch -> Toast.makeText(this, "Search is clicked", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
 }
