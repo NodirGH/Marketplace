2b1105d0fe163a29ade2463df46efca2e112e2d3
Nodir <mahkamov.nodirbek.95@gmail.com>
2022-04-27 19:23:43 +0500

    Horizontal recyclerView (item) is ready

---
+++ app/src/main/java/com/example/marketplace/ItemMenuOrderFragment.kt
@@ -0,0 +1,17 @@
+package com.example.marketplace
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ItemMenuOrderFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
+
