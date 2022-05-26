0ae1d2b3d7b55fa879b16315ded19a7930f19ffc
Nodir <mahkamov.nodirbek.95@gmail.com>
2022-05-26 10:06:40 +0500

    bottom navigation is implied

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -1,26 +1,69 @@
 package com.example.marketplace
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.marketplace.core.BaseActivity
+import com.example.marketplace.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
+    private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, FoodMenuFragment()).commit()
+        binding.bottomNavigation.setOnNavigationItemSelectedListener (navListener)
+
+        val spAdapter = ArrayAdapter(this, R.layout.spinner_color, resources.getStringArray(R.array.orderPlacesSpinner))
+        spAdapter.setDropDownViewResource(R.layout.dropdown_spinner_color)
+        binding.spOrderPlace.adapter = spAdapter
+
+        binding.spOrderPlace.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
     }
 
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId) {
+            R.id.locationFragment -> {
+                currentFragment = LocationFragment()
+            }
+
+            R.id.foodMenuFragment -> {
+                currentFragment = FoodMenuFragment()
+            }
+            R.id.accountFragment -> {
+                currentFragment = AccountFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, currentFragment).commit()
+        true
+    }
 
 }
--- app/src/main/java/com/example/marketplace/MenuFragment.kt
+++ app/src/main/java/com/example/marketplace/MenuFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.marketplace.core.BaseFragment
 import com.example.marketplace.databinding.FragmentMenuFragmentBinding
 
@@ -14,25 +12,9 @@ class MenuFragment : BaseFragment<FragmentMenuFragmentBinding>(FragmentMenuFragm
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.bottomNavigation.setupWithNavController(findNavController())
 
-        val adapter = ArrayAdapter(requireContext(), R.layout.spinner_color, resources.getStringArray(R.array.orderPlacesSpinner))
-        adapter.setDropDownViewResource(R.layout.dropdown_spinner_color)
-        binding.spOrderPlace.adapter = adapter
 
-        binding.spOrderPlace.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                adapterView: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
 
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-        }
     }
 
 }
